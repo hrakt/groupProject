29f12f3c943719ec11fff82458c1f362f846f06a
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-04-24 14:26:06 -0700

    add env

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost/groupProject';
+const connectionString = 
 
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
