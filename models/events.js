d20fe0c6da282a6cb230fc6df5f12789b56f3ef7
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-04-24 15:01:25 -0700

    update model schema for users and events

--- models/events.js
+++ models/events.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const Users = require('./users')
 
 const eventSchema = mongoose.Schema({
     name: String,
-    artists: String,
-    date: String,
+    artists: [{}],
+    date: Date,
     venue: String,
-    picture: String
+    picture: String,
+    users: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Users'
+    }]
 })
 
 const Event = mongoose.model('Event', eventSchema);
--- models/users.js
+++ models/users.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const Events = require('./events');
 
 const userSchema = new mongoose.Schema({
     email:      {type: String, required: true, unique: true},
     firstName:  {type: String, required: true},
     lastName:   String,
     password:   {type: String, required: true},
-})
+    events:     [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Events'
+    }]
+});
 
 const User = mongoose.model('User', userSchema);
 
