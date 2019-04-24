9b6ee2b2d8e99419c205f20463ffe0581b1faf1e
Sky Seulki <admin@Skys-MacBook-Pro.local>
2019-04-24 09:54:07 -0700

    update views and make events model

--- models/events.js
+++ models/events.js
@@ -1,1 +1,12 @@
 const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+    artist: String,
+    date: String,
+    venue: String,
+    picture: String,
+})
+
+const Event = mongoose.model('Event', eventSchema);
+
+module.exports = Event;
