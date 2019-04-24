77044419c65777dc6d39503326a907e8b60d5e82
hrakt <hraktorres@gmail.com>
2019-04-24 10:16:12 -0700

    Add users contorller

--- controllers/users.js
+++ controllers/users.js
@@ -1,2 +1,19 @@
-const express = require('express');
-const router = express.Router();
+const express   = require('express');
+const router    = express.Router();
+const Users     = require('../models/users');
+
+
+router.get('/' , async(req,res) =>{
+    try{
+        // const foundUser = await Users.find({});
+        res.render('users/index.ejs',{
+            // users : foundUsers
+        });
+    }catch(err){
+        console.log('error happening')
+        res.send(err);
+    }
+})
+
+
+module.exports = router;
