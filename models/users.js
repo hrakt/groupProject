ed73e1196bc1f8a5c6818a80d44809ca0a00dc9e
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-04-24 10:10:08 -0700

    add user model password schema

--- models/users.js
+++ models/users.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
-    firstname: {type: String, required: true},
-    lastname: String,
-
+    firstName: {type: String, required: true},
+    lastName: String,
+    password: String
 })
 
 const User = mongoose.model('User', userSchema);
