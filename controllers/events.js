4e3d488d0f134af9fe3a2f7c020683a543a4f1dd
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-04-25 10:02:40 -0700

    add delete button to admin add.ejs

--- controllers/events.js
+++ controllers/events.js
@@ -15,14 +15,6 @@ router.get('/add' , async(req,res) =>{
     }
 });
 
-
-// const createdEvent = await Events.create(req.body);
-// console.log(createdEvent);
-// res.render('events/add.ejs') 
-//     events: createdEvent
-//     console.log(`==================== ${events}`)
-// res.redirect('/events/add')
-
 router.get('/index', async(req,res)=>{
     try{
         const foundEvents = await Events.find({});
@@ -47,6 +39,17 @@ router.post('/add', async(req,res)=>{
     
 });
 
+router.delete('/add', async (req, res) => {
+    try {
+        const deletedEvent = await Events.findByIdAndRemove(req.params.id);
+        const foundUser = await Events.findOne({'events': req.params.id})
+        foundUser.events.remove(req.params.id);
+        foundUser.save();
+        res.redirect('/events/add');
+    } catch (err){
+        res.send(err);
+    }
+})
 
 
 module.exports = router;
