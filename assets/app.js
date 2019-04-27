6e1aca39292c9d2ef1ffa89e626f2dc83a3e39af
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-04-27 13:22:48 -0700

    add images and text to homepage

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,21 @@
 console.log('js connected');
 
-function onReady(check) {
-    const intervalID = window.setInterval(checkReady, 1000);
+// function onReady(check) {
+//     const intervalID = window.setInterval(checkReady, 1000);
 
-    function checkReady() {
-        if (document.querySelector('.home-container')[0] !== undefined) {
-            window.clearInterval(intervalID);
-            check.call(this);
-        }
-    }
-}
+//     function checkReady() {
+//         if (document.querySelector('.home-container')[0] !== undefined) {
+//             window.clearInterval(intervalID);
+//             check.call(this);
+//         }
+//     }
+// }
 
-function show(id, value) {
-    document.getElementById(id).style.display = value ? 'flex' : 'none';
-}
+// function show(id, value) {
+//     document.getElementById(id).style.display = value ? 'flex' : 'none';
+// }
 
-onReady(function () {
-    show('.home-container', true);
-    show('.loader-container', false);
-});
+// onReady(function () {
+//     show('.home-container', true);
+//     show('.loader-container', false);
+// });
