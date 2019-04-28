bec6ce194b1a3011426607c002861ee657743f43
Rogelio Hidalgo <rhidalgo@Rogelios-MacBook-Pro.local>
2019-04-27 18:13:12 -0700

    add hover over effect to EYE text, on hover image will change

--- assets/app.js
+++ assets/app.js
@@ -19,3 +19,43 @@ console.log('js connected');
 //     show('.home-container', true);
 //     show('.loader-container', false);
 // });
+
+const exploreText = document.querySelector('.explore-static');
+const exploreImage = document.querySelector('.home-img-explore')
+
+const yourText = document.querySelector('.your-static')
+const yourImage = document.querySelector('.home-img-your')
+
+const elseText = document.querySelector('.else-static')
+const elseImage = document.querySelector('.home-img-else')
+console.log(exploreText)
+
+exploreText.addEventListener('mouseover', function() {
+    exploreImage.style.zIndex = '0';
+    exploreImage.style.animation = 'fadein 1s linear 1';
+    exploreText.addEventListener('mouseout', function() {
+    exploreImage.style.zIndex = '0';
+    exploreImage.style.animation = '';
+    })
+    console.log(exploreText)
+})
+
+yourText.addEventListener('mouseover', function() {
+    yourImage.style.zIndex = '0';
+    yourImage.style.animation = 'fadein 1s linear 1';
+    yourText.addEventListener('mouseout', function() {
+    yourImage.style.zIndex = '-1';
+    yourImage.style.animation = '';
+    })
+    console.log(yourText)
+})
+
+elseText.addEventListener('mouseover', function() {
+    elseImage.style.zIndex = '0';
+    elseImage.style.animation = 'fadein 1s linear 1';
+    elseText.addEventListener('mouseout', function() {
+    elseImage.style.zIndex = '-2';
+    elseImage.style.animation = '';
+    })
+    console.log(elseText)
+})
